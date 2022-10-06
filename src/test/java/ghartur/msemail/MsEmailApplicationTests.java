8624999244560a3725159010b41c4e48ea943827
Artur Santos <arturfrancisco86@gmail.com>
2022-10-05 21:53:43 -0300

    Projeto iniciado"

---
+++ src/test/java/ghartur/msemail/MsEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package ghartur.msemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
