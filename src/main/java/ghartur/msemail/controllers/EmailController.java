868357397dc62a82c7a342d1556ee8c8db9b784f
Artur Santos <arturfrancisco86@gmail.com>
2022-10-09 20:56:46 -0300

    Microsserviço testado e funcionando, proximo passo é a integração com o RabbitMQ

--- src/main/java/ghartur/msemail/controllers/EmailController.java
+++ src/main/java/ghartur/msemail/controllers/EmailController.java
@@ -1,16 +1,32 @@
 package ghartur.msemail.controllers;
 
+import ghartur.msemail.dtos.EmailDto;
 import ghartur.msemail.models.EmailModel;
 import ghartur.msemail.services.EmailService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController()
 public class EmailController {
 
     @Autowired
     EmailService emailService;
+
+@PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto){
+
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+
+    }
 
 }
--- src/main/java/ghartur/msemail/services/EmailService.java
+++ src/main/java/ghartur/msemail/services/EmailService.java
@@ -1,15 +1,42 @@
 package ghartur.msemail.services;
 
+import ghartur.msemail.enums.StatusEmail;
+import ghartur.msemail.models.EmailModel;
 import ghartur.msemail.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
 
     @Autowired
     EmailRepository emailRepository;
+    @Autowired
+    private JavaMailSender emailSender;
 
 
+    public EmailModel sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try{
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(emailModel.getEmailFrom());
+        message.setTo(emailModel.getEmailTo());
+        message.setSubject(emailModel.getSubject());
+        message.setText(emailModel.getText());
+        emailSender.send(message);
 
+        emailModel.setStatusEmail(StatusEmail.SENT);}
+
+        catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(emailModel);
+        }
+
+    }
 }
